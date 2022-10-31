e82e0a775c757b302eb1551d92aa991698fe20e3
Denis.F <kazzzzzatovvv@gmail.com>
2022-10-31 21:32:17 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string path;
+
+    ifstream inputFile;
+
+    do {
+        cout << "Имя файла: ";
+        getline(cin, path);
+        inputFile.open(path.c_str());
+        if (inputFile.is_open()) {
+            break;
+        }
+        cout << "Не удалось найти файл\n" << path << endl;
+    } while (true);
+
+    string tempString;
+
+    size_t stringCount = 0;
+
+    while (getline(inputFile, tempString)) {
+        cout << setw(3) << setfill('0') << right << (++stringCount) << ". " << tempString << "\n";
+    }
+}
